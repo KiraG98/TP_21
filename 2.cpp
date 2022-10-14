46f02849aa80f4fb835124ba252966051f1a6ee1
N7143 <N7143@K310S1.dune.edu>
2022-10-14 19:28:15 +0200

    2 файл

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+bool sqr_lt_2(double x)
+{
+  return x*x < 2;
+}
+
+int main()
+{
+  double x = 0;
+  int l;
+  cout << "Enter a sequence of numbers x: ";
+  while (cin >> x) 
+  {
+    cout << "x*x < 2  == " << sqr_lt_2(x) << endl;
+  }
+  return EXIT_SUCCESS;
+}
