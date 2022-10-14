4d0f8b3549f775d26d47908138b2687e358b42fd
N7143 <N7143@K310S1.dune.edu>
2022-10-14 18:21:41 +0200

    Коммент

--- 1.cpp
+++ 1.cpp
@@ -8,12 +8,11 @@ bool sqr_lt_2(double x)
   return x*x < 2;
 }
 
-
 int main()
 {
   double x = 0;
   cout << "Enter a sequence of numbers x: ";
-  while (cin >> x) // Условие продолжения выполнения цикла.
+  while (cin >> x) 
   {
     cout << "x*x < 2  == " << sqr_lt_2(x) << endl;
   }
